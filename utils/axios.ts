2a5a26095aa849031eec6e1f226767ecb52d126e
cadi <dbtmdgns4296@naver.com>
2025-05-16 21:40:46 +0900

    refactor: 불필요 주석 제거

--- utils/axios.ts
+++ utils/axios.ts
@@ -1,8 +1,7 @@
-// src/utils/axiosInstance.ts
 import axios from "axios";
 
 const axiosInstance = axios.create({
-  baseURL: process.env.NEXT_PUBLIC_API_URL, // Set your API base URL here
+  baseURL: process.env.NEXT_PUBLIC_API_URL,
   headers: {
     "Content-Type": "application/json",
   },
