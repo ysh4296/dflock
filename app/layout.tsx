7d7a9509c6968003d56957c204b0e9918cc7a778
cadi <dbtmdgns4296@naver.com>
2025-05-16 22:45:05 +0900

    feat: env 환경변수 명칭 next 컨벤션에 맞게 수정

--- app/layout.tsx
+++ app/layout.tsx
@@ -7,13 +7,13 @@ export const metadata: Metadata = {
   title: "던파 봉인된 자물쇠 확률 계산기, 통계 시뮬레이터 던락",
   description: "봉인된 자물쇠의 확률을 여러가지 통계 기법을 통해 알아보세요!",
   openGraph: {
-    url: process.env.NEXT_SERVICE_URL ?? "",
+    url: process.env.NEXT_PUBLIC_SERVICE_URL ?? "",
     title: "던파 봉인된 자물쇠 확률 계산기, 통계 시뮬레이터 던락",
     type: "website",
     description: "봉인된 자물쇠의 확률을 여러가지 통계 기법을 통해 알아보세요!",
     images: [
       {
-        url: process.env.NEXT_SERVICE_URL ?? "",
+        url: process.env.NEXT_PUBLIC_SERVICE_URL ?? "",
         width: 800,
         height: 600,
         alt: "Og Image Alt",
